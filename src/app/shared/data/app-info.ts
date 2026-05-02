7a36e5e6a327da43f39afdd5cc0e58b7f231e410
Nidhinraj <nidhinraj.tb08@gmail.com>
2026-05-02 17:27:38 +0530

    version update

--- src/app/shared/data/app-info.ts
+++ src/app/shared/data/app-info.ts
@@ -1,7 +1,7 @@
 export const APP_INFO = {
     name: 'CircleUp',
-    version: '1.1.1',
+    version: '1.1.2',
     buildTime: '26-05-02',
-    buildNumber: 1,
+    buildNumber: 2,
     env: 'prod'
 };
